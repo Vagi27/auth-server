415d65a47442e65418255ecfea559dd7e106f774
Vagish <Vagishmlk@gmail.com>
2024-12-10 07:34:51 +0530

    setup

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const PORT = 3000;
+const HOST = "127.0.0.1";
 
-app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
+app.get("/", (req, res) => {
+    res.send("Server is active");
+});
+
+app.use((req, res) => {
+    return res.status(404).send("page not available on server");
+});
+
+app.listen(PORT, HOST, () => {
+    console.log(` server started @: http://${HOST}:${PORT}`);
 });
