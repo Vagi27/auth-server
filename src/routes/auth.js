9083ba4f911432c584c70482e99b58a27fc32a10
Vagish <Vagishmlk@gmail.com>
2024-12-10 15:24:32 +0530

    router now supports signup and login

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,5 +1,10 @@
 const authRouter = require("express").Router();
 const Encryptor = require("../services/encryptor");
+const User = require("../models/user");
+const jwt = require("jsonwebtoken");
+const JWT_Secret_Key = require("../config/config").JWT_Secret_Key;
+
+
 const {
     validateSignUpData,
     validateLoginData,
@@ -8,15 +13,46 @@ const {
 authRouter.post("/signup", async (req, res) => {
     try {
         validateSignUpData(req);
+        const user = new User(req.body);
+
+        const foundUser = await User.findOne({ username: req.body.username });
+        if (foundUser) {
+            throw new Error("User already exists");
+        }
+        const hashedPassword = await Encryptor.hash(user.password);
+
+        user.password = hashedPassword;
+        user.save();
+        res.json({ message: "User Added Successfully", data: user });
     } catch (err) {
-        res.send("something went wrong in signup");
+        res.send("something went wrong in signup\n " + err.message);
     }
 });
 authRouter.post("/login", async (req, res) => {
     try {
         validateLoginData(req);
+        const { username, password } = req.body;
+
+        const user = await User.findOne({ username });
+        if (!user) {
+            throw new Error("user does not exist!");
+        }
+
+        const match = await Encryptor.compareHash(password, user.password);
+        if (!match) {
+            throw new Error("invalid credentials!");
+        }
+        const payload = {
+            name: user.name,
+            username: user.username,
+            dateOfBirth: user.dateOfBirth,
+        };
+        const token = jwt.sign(payload, JWT_Secret_Key);
+        // console.log(token);
+        res.cookie("token", token);
+        res.send("login successful");
     } catch (err) {
-        res.send("something went wrong in signup");
+        res.send("something went wrong in login\n" + err.message);
     }
 });
 
